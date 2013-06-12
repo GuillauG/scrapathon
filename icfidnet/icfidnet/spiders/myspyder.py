6769f2567aff28c4d5d485d7c0cf54893458fc34
Guillaume Augais <guillaume.augais@gmail.com>
2013-06-12 23:10:58 +0200

    mandats has its own fn

--- icfidnet/icfidnet/spiders/myspyder.py
+++ icfidnet/icfidnet/spiders/myspyder.py
@@ -19,33 +19,38 @@ class MandatItem(Item):
 	pass
 
 class MyspyderSpider(CrawlSpider):
-    name = "myspyder"
-    allowed_domains = ["icfidnet.ansm.sante.fr"]
-    start_urls = ('https://icfidnet.ansm.sante.fr/Public/memb_idx.php',)
+	name = "myspyder"
+	allowed_domains = ["icfidnet.ansm.sante.fr"]
+	start_urls = ('https://icfidnet.ansm.sante.fr/Public/memb_idx.php',)
 
-    rules = [ Rule(SgmlLinkExtractor(allow='membre.php', restrict_xpaths=("/html//a[@class='ifides']")),
-    				follow=False,
-    				callback='parse_member') ]
+	rules = [ Rule(SgmlLinkExtractor(allow='membre.php', restrict_xpaths=("/html//a[@class='ifides']")),
+					follow=False,
+					callback='parse_member') ]
 
-    def parse_member(self, response):
-    	hxs = HtmlXPathSelector(response)
+	def mandat(self, mrow):
+		grp_node = mrow.select("td[1]")
 
-    	fullname = hxs.select("/html/body/h3/text()").extract()[0]
-    	mandat_rows = hxs.select("/html/body/table[5]/tr")
-    	
-    	def mandat(mrow):
-    		grp_node = mrow.select("td[1]")
+		if len(grp_node.select("a").extract()) == 0:
+			r = mrow.select("td/text()").extract()
+			return MandatItem(groupe=r[0], code=r[1], decision=r[2], statut=r[3])
+		else:
+			a = mrow.select("td/a/text()").extract()[0]
+			r = mrow.select("td/text()").extract()
+			return MandatItem(groupe=a, code=r[0], decision=r[1], statut=r[2])
 
-    		if len(grp_node.select("a").extract()) == 0:
-    			r = mrow.select("td/text()").extract()
-    			return MandatItem(groupe=r[0], code=r[1], decision=r[2], statut=r[3])
-    		else:
-    			a = mrow.select("td/a/text()").extract()[0]
-    			r = mrow.select("td/text()").extract()
-    			return MandatItem(groupe=a, code=r[0], decision=r[1], statut=r[2])
+	def extract_mandats(self, hxs):
+		mandat_rows = hxs.select("/html/body/table[5]/tr")
+		mandats = [ self.mandat(mrow) for idx, mrow in enumerate(mandat_rows) ]
 
-    	mandats = [ mandat(mrow) for idx, mrow in enumerate(mandat_rows) ]
+		return mandats
 
-    	i = IcfidnetItem(fullname=fullname, mandats=mandats)
 
-    	return i
+	def parse_member(self, response):
+		hxs = HtmlXPathSelector(response)
+
+		fullname = hxs.select("/html/body/h3/text()").extract()[0]
+		mandats = self.extract_mandats(hxs)
+
+		i = IcfidnetItem(fullname=fullname, mandats=mandats)
+
+		return i
