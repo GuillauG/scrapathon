06706753f1bf661170ba940864eb5038a49b4aad
Guillaume Augais <guillaume.augais@gmail.com>
2013-06-22 13:38:35 +0200

    first fully working scraper

--- README.md
+++ README.md
@@ -2,3 +2,7 @@ scrapathon
 ==========
 
 Scrapathon
+
+```
+scrapy runspider icfidnet/spiders/myspyder.py -o full.json --loglevel INFO
+```
--- icfidnet/icfidnet/spiders/myspyder.py
+++ icfidnet/icfidnet/spiders/myspyder.py
@@ -1,4 +1,3 @@
-from scrapy.spider import BaseSpider
 from scrapy.contrib.spiders import CrawlSpider, Rule
 from scrapy.selector import HtmlXPathSelector
 from scrapy.contrib.linkextractors.sgml import SgmlLinkExtractor
@@ -6,9 +5,9 @@ from scrapy.contrib.linkextractors.sgml import SgmlLinkExtractor
 from scrapy.item import Item, Field
 
 class IcfidnetItem(Item):
-    # define the fields for your item here like:
     fullname = Field()
     mandats = Field()
+    declarations = Field()
     pass
 
 class MandatItem(Item):
@@ -16,6 +15,19 @@ class MandatItem(Item):
 	code = Field()
 	decision = Field()
 	statut = Field()
+	pass
+
+class DeclarationItem(Item):
+	date = Field()
+	content = Field()
+	pass
+
+class DeclarationContentItem(Item):
+	typ = Field()
+	entreprise = Field()
+	activite = Field()
+	participation = Field()
+	periode = Field()
 	pass
 
 class MyspyderSpider(CrawlSpider):
@@ -44,13 +56,57 @@ class MyspyderSpider(CrawlSpider):
 
 		return mandats[1:]
 
+	def make_declaration(self, hnode, dnode):
+		header = hnode.select("tr/td/b/text()").extract()[0]
+		decl_rows = dnode.select("tr[position() >= 2]")
+
+		def from_row(decl_row):
+			decl = decl_row.select("td")
+			l = len(decl)
+			
+			if l <= 1:
+				# Declaration est "Neant (declaration d'absence de liens)"
+				return DeclarationItem(date=header, content=None)
+			else:
+				# Declaration non vide
+
+				# recupere le type et l'entreprise declaree
+				typ = decl_row.select("td[1]/a/b/text()").extract()[0]
+				ent = decl_row.select("td[2]/b/text()").extract()[0]
+				
+				# autres champs : activite/produits, participation/remun et periode
+				r = decl_row.select("td[position() > 2]/text()").extract()
+
+				# si la particpation n'est pas mentionnee
+				if len(r) == 2:
+					act, part, peri = r[0], "unkown", r[1]
+				# si la particpation est mentionnee
+				else:
+					act, part, peri = r[0], r[1], r[2]
+				
+				dContent = DeclarationContentItem(typ=typ, entreprise=ent, activite=act, participation=part, periode=peri)
+				return DeclarationItem(date=header, content=dContent)
+
+
+		return [ from_row(decl_row) for i, decl_row in enumerate(decl_rows) ]
+
+	def extract_declaration(self, hxs):
+		decls = hxs.select("/html/body/table[@width='80%']") # remove mandats
+
+		# pages ou liens declares est "AUCUN DECLARATION N A ETE ENREGISTREE DANS FIDES"
+		if len(decls) == 3:
+			return
+
+		grps = [ (decls[i], decls[i+1]) for i, d in enumerate(decls) if (i % 2 == 0) & (i > 1)]
+
+		return [ self.make_declaration(hnode, dnode) for hnode, dnode in grps ]
+
 
 	def parse_member(self, response):
 		hxs = HtmlXPathSelector(response)
 
 		fullname = hxs.select("/html/body/h3/text()").extract()[0]
 		mandats = self.extract_mandats(hxs)
+		declarations = self.extract_declaration(hxs)
 
-		i = IcfidnetItem(fullname=fullname, mandats=mandats)
-
-		return i
+		return IcfidnetItem(fullname=fullname, mandats=mandats, declarations=declarations)
